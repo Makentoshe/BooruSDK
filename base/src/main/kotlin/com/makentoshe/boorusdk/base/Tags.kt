b6af2e94f04482e0a8fa67013bd6fb3d30129c23
Makentoshe <mkliverout@gmail.com>
2019-09-03 22:44:53 +0300

    [refactor](base) Make Tags class inline

--- base/src/main/kotlin/com/makentoshe/boorusdk/base/Tags.kt
+++ base/src/main/kotlin/com/makentoshe/boorusdk/base/Tags.kt
@@ -1,6 +1,6 @@
 package com.makentoshe.boorusdk.base
 
-class Tags(val value: Set<String> = setOf()) {
+inline class Tags(val value: Set<String> = setOf()) {
     override fun toString(): String {
         return value.joinToString(separator = " ")
     }
