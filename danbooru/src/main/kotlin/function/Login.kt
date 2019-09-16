f491fcf5a5876b73fa207cb4396743406cede0a8
Makentoshe <mkliverout@gmail.com>
2019-09-16 17:39:03 +0300

    [refactor] Move Login logic to another class

---
+++ danbooru/src/main/kotlin/function/Login.kt
@@ -0,0 +1,53 @@
+package function
+
+import DanbooruApi
+import com.makentoshe.boorusdk.base.request.LoginRequest
+import org.jsoup.Jsoup
+import retrofit2.Response
+import java.util.function.Function
+
+/**
+ * Performs a log in action for danbooru
+ */
+internal class Login(
+    private val danbooruApi: DanbooruApi
+) : Function<LoginRequest, Boolean> {
+
+    /**
+     * Performs a log in action
+     * @return true if log in success and false otherwise
+     */
+    override fun apply(t: LoginRequest): Boolean {
+        return danbooruApi.login(
+            token = getAuthenticityToken(),
+            username = t.username,
+            password = t.password
+        ).execute().let { response ->
+            response.hasUserName() && response.hasPasswordHash()
+        }
+    }
+
+    private fun getAuthenticityToken(): String {
+        val httpLoginPage = danbooruApi.newSession().execute().extractBody()
+        return Jsoup.parse(httpLoginPage).select("[name=authenticity_token]").attr("value")
+    }
+
+    private fun Response<ByteArray>.hasUserName(): Boolean {
+        val cookies = raw().priorResponse?.headers("set-cookie")
+        return cookies?.any { it.contains("user_name") } ?: false
+    }
+
+    private fun Response<ByteArray>.hasPasswordHash(): Boolean {
+        val cookies = raw().priorResponse?.headers("set-cookie")
+        return cookies?.any { it.contains("password_hash") } ?: false
+    }
+
+    private fun Response<ByteArray>.extractBody(): String {
+        return if (isSuccessful) {
+            String(body() ?: byteArrayOf())
+        } else {
+            throw Exception(errorBody()?.string() ?: message())
+        }
+    }
+
+}
